1b53892e7763c0fe9fa5f62bd21acc45a057fa6b
Ryan Furman <rfurman3803@gmail.com>
2025-06-10 18:45:16 -0500

    Restructred files for new additions

---
+++ aws/main aws.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "aws_vm" {
+  ami           = "ami-0c94855ba95c71c99" # Amazon Linux 2 (us-east-1)
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "AWS_VM"
+  }
+}
+
--- gcp/main.tf
+++ gcp/main.tf
@@ -1,12 +1,3 @@
-resource "aws_instance" "aws_vm" {
-  ami           = "ami-0c94855ba95c71c99" # Amazon Linux 2 (us-east-1)
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "AWS_VM"
-  }
-}
-
 resource "google_compute_instance" "gcp_vm" {
   name         = "gvp-vm"
   machine_type = "e2-micro"
