658fb406400655ae03e3ef9f369cb2a7b6d20c1a
Ryan Furman <rfurman3803@gmail.com>
2025-06-10 20:23:48 -0500

    updated variables for aws

--- main.tf
+++ main.tf
@@ -1,7 +1,12 @@
 module "aws" {
-  source = "/aws"
+  source          = "./aws"
+  aws_region      = var.aws_region
+  aws_access_key  = var.aws_access_key
+  aws_secret_key  = var.aws_secret_key
 }
 
 module "gcp" {
-  source = "/gcp"
+  source               = "./gcp"
+  gcp_project_id       = var.gcp_project_id
+  gcp_credentials_file = var.gcp_credentials_file
 }
