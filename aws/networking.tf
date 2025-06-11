03f6934e8e89ddcb7b417e319d87a69b5a72f423
Ryan Furman <rfurman3803@gmail.com>
2025-06-10 20:38:06 -0500

    fixed bug

--- aws/networking.tf
+++ aws/networking.tf
@@ -14,7 +14,7 @@ resource "aws_vpc" "main" {
 #create subnet
 #ec2 launches with public ip auto
 resource "aws_subnet" "public_subnet" {
-    vpc_id = aws_vpc.main
+    vpc_id = aws_vpc.main.id
     cidr_block = "10.0.1.0/24"
     map_public_ip_on_launch = true
     availability_zone = "us-east-1a"
