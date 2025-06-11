aa5d59e53590c04f794a5c884c8b463566244b74
Ryan Furman <rfurman3803@gmail.com>
2025-06-10 19:20:52 -0500

    Created security.tf framework

---
+++ aws/security.tf
@@ -0,0 +1,28 @@
+#create security group
+resource "aws_security_group" "vm_sg" {
+  name = "vm_security_group"
+  description = "Allow SSH and HTTP access"
+  vpc_id = aws_vpc.main.id
+
+#create incoming rules
+  ingress {
+    description = "HTTP access"
+    from_port =  80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+#outbound rules
+  egress {
+    description = "Allow all outbound"
+    from_port =  0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "vm_security_group"
+  }
+}
