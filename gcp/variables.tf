67f0c059d22d9577505b4059b7757d6060f9d152
Ryan Furman <rfurman3803@gmail.com>
2025-06-10 20:11:21 -0500

    creation of main rootfile

---
+++ gcp/variables.tf
@@ -0,0 +1,9 @@
+variable "gcp_project_id" {
+  description = "multi-cloud-terraform"
+  type        = string
+}
+
+variable "gcp_credentials_file" {
+  description = "Path to GCP service account JSON key"
+  type        = string
+}
